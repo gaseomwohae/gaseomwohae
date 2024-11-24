c8b823a07ea6ca1efd753b7fab955a6d1ea740ad
minjumost <95904729+minjumost@users.noreply.github.com>
2024-11-24 13:08:29 +0900

    project init

---
+++ src/main/java/com/gaseomwohae/gaseomwohae/GaseomwohaeApplication.java
@@ -0,0 +1,13 @@
+package com.gaseomwohae.gaseomwohae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaseomwohaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaseomwohaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaseomwohae/gaseomwohae/GaseomwohaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaseomwohae.gaseomwohae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaseomwohaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
