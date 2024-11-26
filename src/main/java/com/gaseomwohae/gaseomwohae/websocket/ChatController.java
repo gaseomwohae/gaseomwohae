4f0841db7f9fe2831b35355961686a2780d43852
minjumost <minjumost@naver.com>
2024-11-26 20:25:26 +0900

    feat: web sockket

---
+++ src/main/java/com/gaseomwohae/gaseomwohae/websocket/ChatController.java
@@ -0,0 +1,15 @@
+package com.gaseomwohae.gaseomwohae.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public String sendMessage(String message) {
+        return message;
+    }
+}
